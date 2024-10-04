097b96ecd12e5193c0e61cc6df14040f4e9b6ae2
eugenherman <omenapuu1000@outlook.com>
2024-10-04 18:01:20 +0300

    jsb

---
+++ Desktop/backendjs/jsb.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/db.config');
+
+// Define a User model
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+});
+
+module.exports = User;
